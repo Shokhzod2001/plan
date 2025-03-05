ae03cf0b8c23ab50f9835bef9c4ec5dcb25ad5d2
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-03-06 02:59:20 +0900

    BRR: CRUD Update section in plan

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post("/edit-item", (req, res) => {
 app.post("/delete-all", (req, res) => {
   if (req.body.delete_all) {
     db.collection("plans").deleteMany(function (err, data) {
-      res.json({ state: "delete all plans" });
+      res.json({ state: "delete all plans!" });
     });
   }
 });
